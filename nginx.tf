da37831436874f2a4ba476ae83571dd3301b6bbe
Mauricio Martinez <n_othing@hotmail.com>
2020-01-02 15:00:11 -0600

    Nginx Ingress

--- nginx.tf
+++ nginx.tf
@@ -12,6 +12,15 @@ resource "kubernetes_namespace" "nginx_ingress" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host = google_container_cluster.cluster.endpoint
+
+    client_certificate     = google_container_cluster.cluster.master_auth.0.client_certificate
+    client_key             = google_container_cluster.cluster.master_auth.0.client_key
+    cluster_ca_certificate = google_container_cluster.cluster.master_auth.0.cluster_ca_certificate
+  }
+}
 
 resource "helm_release" "nginx_ingress" {
   name  = "nginx-ingress"
