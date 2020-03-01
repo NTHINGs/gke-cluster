ef1558af5b6997092b63434f47fc1fd8fc7b83cf
Mauricio Martinez <n_othing@hotmail.com>
2020-03-01 16:11:37 -0600

    Fixing nginx

--- nginx.tf
+++ nginx.tf
@@ -1,10 +1,10 @@
 data "google_client_config" "current" {}
 
 provider "kubernetes" {
-  load_config_file = false
-  host = "https://${google_container_cluster.cluster.endpoint}"
+  load_config_file       = false
+  host                   = "https://${google_container_cluster.cluster.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
-  token = data.google_client_config.current.access_token
+  token                  = data.google_client_config.current.access_token
 }
 
 resource "kubernetes_namespace" "nginx_ingress" {
@@ -15,16 +15,20 @@ resource "kubernetes_namespace" "nginx_ingress" {
 
 provider "helm" {
   kubernetes {
-    load_config_file = false
-    host = "https://${google_container_cluster.cluster.endpoint}"
+    load_config_file       = false
+    host                   = "https://${google_container_cluster.cluster.endpoint}"
     cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
-    token = data.google_client_config.current.access_token
+    token                  = data.google_client_config.current.access_token
   }
 }
 
 resource "helm_release" "nginx_ingress" {
-  name  = "nginx-ingress"
-  chart = "stable/nginx-ingress"
+  name          = "nginx-ingress"
+  repository    = "https://kubernetes-charts.storage.googleapis.com"
+  chart         = "stable/nginx-ingress"
+  namespace     = kubernetes_namespace.nginx_ingress.id
 
-  namespace = kubernetes_namespace.nginx_ingress.id
+  values = [
+    file("${path.module}/values.yaml"),
+  ]
 }
