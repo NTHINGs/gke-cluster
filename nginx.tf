b05f0012e6885672398f84550781f1f3900d4bbe
Mauricio Martinez <n_othing@hotmail.com>
2020-03-01 18:35:18 -0600

    Correcting this

---
+++ nginx.tf
@@ -0,0 +1,40 @@
+data "google_client_config" "current" {}
+
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = "https://${google_container_cluster.cluster.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
+  token                  = data.google_client_config.current.access_token
+}
+
+resource "kubernetes_namespace" "nginx_ingress" {
+  metadata {
+    name = "nginx-ingress"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    host                   = "https://${google_container_cluster.cluster.endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
+    token                  = data.google_client_config.current.access_token
+  }
+}
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+resource "helm_release" "nginx_ingress" {
+  name          = "nginx-ingress"
+  repository    = data.helm_repository.stable.metadata[0].name
+  chart         = "nginx-ingress"
+  version       = "1.33.0"
+  namespace     = kubernetes_namespace.nginx_ingress.id
+
+  values = [
+    file("${path.module}/values.yaml"),
+  ]
+}
